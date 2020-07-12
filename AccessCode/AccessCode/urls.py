b5966780c924a7e11e042017a59ee0ec4af33217
chenzijie-chenxiaoli-chentong <412037638@qq.com>
2020-07-13 00:04:46 +0800

    接码

--- AccessCode/AccessCode/urls.py
+++ AccessCode/AccessCode/urls.py
@@ -1,4 +1,4 @@
-"""接码平台 URL Configuration
+"""accessCode URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
-from AccessCode import views
+from code import views
 
 urlpatterns = [
     # path('admin/', admin.site.urls),  # 博客后台登录页
--- AccessCode/code/apps.py
+++ AccessCode/code/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class AccesscodeConfig(AppConfig):
-    name = 'AccessCode'
+    name = 'acceCode'
